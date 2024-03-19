526da60ef93481c29b407e4d5ac3b49850f97f03
Kevin McCormack <kevin@mccormack.tech>
2024-03-19 08:58:26 -0400

    Lint fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+##
+# Top level helper
+#
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+##
+# Top level model class
+#
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
