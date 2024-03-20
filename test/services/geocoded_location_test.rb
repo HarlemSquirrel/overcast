132433092ae23f075a5174dcf397269fe3459e92
Kevin McCormack <kevin@mccormack.tech>
2024-03-20 10:34:27 -0400

    Add geocode location test

---
+++ test/services/geocoded_location_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class GeocodedLocationTest < ActiveSupport::TestCase
+  test "find by address" do
+    geocoded_location = GeocodedLocation.new('1 times square, new york, ny')
+    assert geocoded_location.found?
+    assert_equal '10036', geocoded_location.postal_code
+    assert_equal 'us', geocoded_location.country_code
+  end
+end
